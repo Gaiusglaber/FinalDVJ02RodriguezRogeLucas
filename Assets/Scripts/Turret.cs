ff9ca735ab5e6fc016fa25bcf85d0b5146f8f471
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-07-30 17:11:12 -0300

    Turret rotation added

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour,IShootable
+{
+    public float speed;
+    private float angle;
+    void RotateTurret()
+    {
+        angle += Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        Mathf.Clamp(angle, 0, 360);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RotateTurret();
+    }
+    public void Shoot()
+    {
+
+    }
+}
