383591f073f1b45effcc41331edebbd303484b05
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-07-30 13:44:46 -0300

    Tank movement added

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2 distance;
+    public Player player;
     void Start()
     {
-        
+        player.OnFollowingPlayer += FollowPlayer;
     }
-
+    private void OnDestroy()
+    {
+        player.OnFollowingPlayer -= FollowPlayer;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    void FollowPlayer()
+    {
+        Vector3 pos;
+        pos = player.transform.position;
+        pos.z += distance.x;
+        pos.y -= distance.y;
+        transform.position = pos;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour,IDestructible
 {
+    public float speed;
     public delegate void OnDestroyed();
     public event OnDestroyed OnPlayerDestroyed;
+    public delegate void Following();
+    public event Following OnFollowingPlayer;
     void Start()
     {
-
+        
     }
-
+    void MovementManager()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        if (movementDirection != Vector3.zero)
+        {
+            transform.forward = movementDirection;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        OnFollowingPlayer?.Invoke();
+        MovementManager();
     }
     void OnDestroy()
     {
