f819ed1380ad9ac1dea0271e6573117cdf35850b
Lucas Rodriguez Roge <lucas.rodriguez.roge@gmail.com>
2021-08-04 10:06:01 -0300

    Changes have been made
    - change turret rotation mechanic
    - deleted 2gb of innecesary assets

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,32 +4,53 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour,IShootable
 {
+    public IEnumerator activeCourutine;
+    public float rotationspeed;
     public GameObject ball;
     public Transform spawnPoint;
     public float rangeX = 15f;
     public float speed;
     private float angle;
-    void RotateTurret()
-    {
-        angle += Input.GetAxis("Mouse X") * speed * Time.deltaTime;
-        Mathf.Clamp(angle, 0, 360);
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
-    }
+    public LayerMask layer;
     // Update is called once per frame
     void Update()
     {
         if (GameManager.GetInstance() != null && !GameManager.GetInstance().pause)
         {
-            RotateTurret();
-            Shoot();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit raycasthit, float.MaxValue, layer))
+                {
+                    Vector3 dir = raycasthit.point;
+                    AimToShoot(dir);
+                }
+            }
         }
+    }
+    public void AimToShoot(Vector3 dir)
+    {
+        StartCoroutine(AimTurret(dir));
+    }
+
+    IEnumerator AimTurret(Vector3 dir)
+    {
+        Quaternion startingRotation = transform.rotation;
+        Quaternion finalRotation = Quaternion.identity;
+        Vector3 aimDirection = dir - transform.position;
+        finalRotation.SetLookRotation(aimDirection, transform.up);
+        float t = 0;
+        while (t < 1)
+        {
+            transform.rotation = Quaternion.Slerp(startingRotation, finalRotation, t);
+            t += Time.deltaTime * rotationspeed;
+            yield return null;
+        }
+        Shoot();
     }
     public void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject instanciateBall = Instantiate(ball,spawnPoint.position,transform.rotation);
-            instanciateBall.GetComponent<Rigidbody>().AddForce(spawnPoint.forward*rangeX, ForceMode.Impulse);
-        }
+        GameObject instanciateBall = Instantiate(ball,spawnPoint.position,transform.rotation);
+        instanciateBall.GetComponent<Rigidbody>().AddForce(spawnPoint.forward*rangeX, ForceMode.Impulse);
     }
 }
